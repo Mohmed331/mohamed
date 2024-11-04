5f38ee3a6e14715ff118cc8603fea6802f25874d
Smart <Mohamedzbedat77@gmail.com>
2024-11-04 12:47:32 +0200

    first ccc

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -12,19 +12,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView etWelcome;
+    private boolean isOn = true;
+    private int mode = 1;
+    private TextView tvmode,tvtemp;
+
+    private int temp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etWelcome=findViewById(R.id.text);
-    }
-    public void change_text(View view){
-        etWelcome.setText("first App");
-    }
-    public void finishApp(View view){
-        finish();
-        System.exit(0);
+        tvmode = findViewById(R.id.tvmode);
+        temp = 24;
+        tvtemp=findViewById(R.id.tvTemp);
+
+
+
     }
 
+
+    public void checkMode(View view) {
+        if (isOn&&mode==1) {
+            mode = 2;
+
+            tvmode.setText("heat");
+        } else {
+            mode = 1;
+
+            tvmode.setText("cool");
+        }
+    }
+    public void checkstatus(View view) {
+        if (isOn) {
+            isOn = false;
+        } else {
+            isOn = true;
+        }
+    }
+
+    public void m1(View view) {
+        if (  isOn&&temp > 16 ) {
+            temp--;
+            tvtemp.setText(String.valueOf(temp));
+        }
+    }
+    public void pu1(View view) {
+        if (  isOn&&temp < 30 ) {
+            temp++;
+            tvtemp.setText(String.valueOf(temp));
+        }
+    }
 }
